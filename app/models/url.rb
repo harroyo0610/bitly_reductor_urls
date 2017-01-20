d72d9b5f894db03f5f932d845df2378f069e5a50
harroyo0610 <humberto-_-lzc@hotmail.com>
2017-01-20 12:58:57 -0600

    Ejercico de bitly

---
+++ app/models/url.rb
@@ -0,0 +1,17 @@
+class Url < ActiveRecord::Base
+#p validates_format_of :long_url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  
+  before_create :url_short
+
+  private
+  def url_short
+    url_final = ""
+    spliteado = @@long.split('/',5)
+    spliteado.each do |i|
+      if i == spliteado[-1]
+        url_final += "#{Time.now.to_s.delete(" -0600")}"        
+      end
+    end
+    self.short_url = url_final
+  end
+end
